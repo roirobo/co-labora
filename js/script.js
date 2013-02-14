0f9ed76addef96624db56de240550caec0cd5f1f
xamanu <drupal@delattre.de>
2013-02-14 16:12:06 -0600

    Exchanging background images to our ones

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   
-	$.backstretch([	"/images/1-casco-0.jpg", 
-			"/images/2-madrid-1.jpg",
-			"/images/3-casco-2.jpg",
-			"/images/4-casco-1.jpg",
-			"/images/5-casco-3.jpg",
+	$.backstretch([	"/images/0.jpg", 
+			"/images/1.jpg",
+			"/images/2.jpg",
+			"/images/3.jpg",
+			"/images/4.jpg",
+			"/images/5.jpg",
 					
 					], {duration: 7000, fade: 750});
 });
